5075889f5a76422246becad3ab73b981ad2e6e46
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2025-03-18 16:28:00 +0700

    Async Code

---
+++ src/async.js
@@ -0,0 +1,11 @@
+export const sayHelloAsync = (name) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (name) {
+        resolve(`Hello ${name}`);
+      } else {
+        reject("Name is empty");
+      }
+    }, 1000); // setelah 1 detik kode dieksekusi
+  });
+};
