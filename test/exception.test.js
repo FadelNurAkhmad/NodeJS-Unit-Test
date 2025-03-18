8fb03ee536aed9c93082a2010b6c438e2f9447f7
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2025-03-18 15:30:36 +0700

    Update Learn NPM

---
+++ test/exception.test.js
@@ -0,0 +1,7 @@
+import { callMe, MyException } from "../src/exception.js";
+
+test("exception", () => {
+  expect(() => callMe("Eko")).toThrow();
+  expect(() => callMe("Eko")).toThrow(MyException);
+  expect(() => callMe("Eko")).toThrow("Ups my exceptions happens");
+});
