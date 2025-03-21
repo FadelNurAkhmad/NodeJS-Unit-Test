095dfd972720d35c86c41f3deb0d845acf040b09
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2025-03-21 15:41:41 +0700

    code coverage

--- test/exception.test.js
+++ test/exception.test.js
@@ -5,3 +5,7 @@ test("exception", () => {
   expect(() => callMe("Eko")).toThrow(MyException);
   expect(() => callMe("Eko")).toThrow("Ups my exceptions happens");
 });
+
+test("exception not happens", () => {
+  expect(callMe("Budi")).toBe("OK");
+});
