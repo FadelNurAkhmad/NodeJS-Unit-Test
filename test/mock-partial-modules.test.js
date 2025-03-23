8c2f41f86c53ec532e9b7d62a5c292606d1c8334
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2025-03-23 23:10:38 +0700

    Mock Modules

---
+++ test/mock-partial-modules.test.js
@@ -0,0 +1,36 @@
+import { getAllProducts, getProductById } from "../src/database.js";
+import { ProductService } from "../src/product-service.js";
+
+jest.mock("../src/database.js", () => {
+  const originalModule = jest.requireActual("../src/database.js");
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    getAllProducts: jest.fn(),
+  };
+  // yang di mock adlah getAllProducts, selain itu akan disimpan di originalModule
+});
+
+test.failing("mock modules getProductById", () => {
+  ProductService.findById(1);
+});
+
+test("mock modules getAllProducts", () => {
+  const products = [
+    {
+      id: 1,
+      name: "Product Mock",
+    },
+    {
+      id: 2,
+      name: "Product Mock",
+    },
+  ];
+
+  getAllProducts.mockImplementation(() => {
+    return products;
+  });
+
+  expect(ProductService.findAll()).toEqual(products);
+});
